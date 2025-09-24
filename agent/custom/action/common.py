3b24faf16722822b2d3585571f047d0250153e01
l0rraine <idn.lee@gmail.com>
2025-09-24 18:16:37 +0800

    feat: 增加启动后确保有队列可用

--- agent/custom/action/common.py
+++ agent/custom/action/common.py
@@ -44,3 +44,78 @@ class SwitchCharacter(CustomAction):
                 })
         print("SwitchCharacter:", json_data)
         return CustomAction.RunResult(success=True)
+
+# 确保所有任务执行前有队列可用，1. 关闭自动加入 2.如果有不是挖矿的队伍，等待  3. 如果全部在挖矿，召回最后一队
+@AgentServer.custom_action("确保有队列可用")
+class MakeSureQueueAvailable(CustomAction):
+    def run(
+        self,
+        context: Context,
+        argv: CustomAction.RunArg,
+    ) -> bool:
+        # 总区域 "roi" : [11,196,230,409]
+        # 1. 关闭自动加入
+        print("closing auto join")
+        context.run_task("关闭自动加入集结入口")
+        context.run_task("转到城外")
+        img = context.tasker.controller.post_screencap().wait().get()
+        detail = context.run_recognition("当前队列已满", img)
+        if detail is not None:
+            a, b = map(int, detail.best_result.text.split('/'))
+            print("cancel a queue")
+            # 2.如果有不是挖矿的队伍，等待 
+            action_region = [
+                [56,546,96,28],
+                [56,486,96,28],
+                [56,426,96,28],
+                [56,366,96,28],
+                [56,306,96,28],
+                [56,246,96,28],
+            ]
+            flag = 0
+            for region in action_region[-b:]:
+                detail = context.run_recognition("识别队列动作", img, {
+                    "识别队列动作":{
+                        "roi": region
+                    }
+                })
+                if detail is not None:
+                    flag = 1
+                    break
+            print("current queue flag:", flag)
+            while flag == 1:
+                time.sleep(3)
+                img = context.tasker.controller.post_screencap().wait().get()
+                detail = context.run_recognition("当前队列已满", img)
+                if detail is None:
+                    break
+            
+            # 3. 如果全部在挖矿，召回最后一队
+            if flag == 0:
+                recall_region = [
+                    [200,544,43,56],
+                    [200,484,43,56],
+                    [200,424,43,56],
+                    [200,364,43,56],
+                    [200,304,43,56],
+                    [200,244,43,56]
+                    
+                ]
+                img = context.tasker.controller.post_screencap().wait().get()
+                for region in recall_region[-b:]:
+                    context.run_task("点击召回",{
+                        "点击召回": {
+                            "target": region
+                        }
+                    })
+                    break
+                print("recalled mine queue, waiting")
+                while True:
+                    time.sleep(3)
+                    img = context.tasker.controller.post_screencap().wait().get()
+                    detail = context.run_recognition("当前队列已满", img)
+                    if detail is None:
+                        break
+
+        
+        return CustomAction.RunResult(success=True)
