854cf8df1861f4b9f5985cc86b9c8402bcfd018c
l0rraine <idn.lee@gmail.com>
2026-01-04 00:28:36 +0800

    chore: minor fix

--- agent/custom/action/__init__.py
+++ agent/custom/action/__init__.py
@@ -2,17 +2,24 @@ from .combat import *
 from .common import *
 from .unite import *
 from .mine import *
+from .monster import *
+from .jina import *
+from .light import *
+from .beast import *
+from .travel import *
 
 __all__ = [
     "ChangeTeam",
-    "ChangeMonsterLevel",
-    "ChooseMonster",
-    "BeginCombat",
+    "BeginCombat", 
     "LightBeginCombat",
     "BeastBeginCombat",
-    
     "RecallTeam",
+    "UniteScan",
+    "MakeSureQueueAvailable",
+    "RecallAllQueue",
+    "JinaCombat",
+    "SetMonsterCount",
+    "RecoVigor",
     "SwitchCharacter",
-    
-    "UniteScan"
+    "DoDig"
 ]
