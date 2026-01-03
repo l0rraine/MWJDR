7a3c250736c45a2e4e7ea570acd17c735849f93a
l0rraine <idn.lee@gmail.com>
2026-01-04 00:20:54 +0800

    chore: 重构

---
+++ agent/custom/action/light.py
@@ -0,0 +1,34 @@
+from maa.agent.agent_server import AgentServer
+from maa.custom_action import CustomAction
+from maa.context import Context
+import time
+
+from utils import timelib
+
+@AgentServer.custom_action("灯塔开始出征")
+class LightBeginCombat(CustomAction):
+    def run(
+        self,
+        context: Context,
+        argv: CustomAction.RunArg,
+    ) -> bool:
+        img = context.tasker.controller.post_screencap().wait().get()             
+        hours, minutes, seconds = timelib.get_time_from_ocr(context,img,"识别集结时间")
+                
+        return_time = hours * 3600 + minutes * 60 + seconds
+        
+        # 开始出征
+        context.run_task("点击出征")
+        time.sleep(0.5)
+        img = context.tasker.controller.post_screencap().wait().get()
+        detail = context.run_recognition("体力不足", img)
+        if detail.hit:
+            detail = context.run_recognition("是否有免费体力",img)
+            if detail.hit:
+                context.run_task("免费体力")
+                context.run_task("点击出征")
+            else:
+                return CustomAction.RunResult(success=True)
+        time.sleep(return_time*2 + 0.5)
+        context.run_task("灯塔入口")        
+        return CustomAction.RunResult(success=True)
