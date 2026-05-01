dbea49cf43feed68bffcc1ddb9d8b03258f1c75f
Z User <z@container>
2026-05-01 07:28:53 +0000

    refactor: click_rect和random_click_point支持Rect和列表两种参数格式
    添加_to_rect辅助函数，将Rect对象和[x,y,w,h]列表统一转换，
    现在可以直接写click_rect(context, [465, 768, 100, 44])

--- agent/utils/click_util.py
+++ agent/utils/click_util.py
@@ -12,23 +12,39 @@ from maa.define import Rect
 from .logger import logger
 
 
-def random_click_point(box: Rect) -> tuple[int, int]:
+def _to_rect(box: Rect | list) -> Rect:
+    """
+    将 Rect 或 [x, y, w, h] 列表统一转换为 Rect
+
+    Args:
+        box: Rect 对象或 [x, y, w, h] 列表
+
+    Returns:
+        Rect 对象
+    """
+    if isinstance(box, list | tuple):
+        return Rect(box[0], box[1], box[2], box[3])
+    return box
+
+
+def random_click_point(box: Rect | list) -> tuple[int, int]:
     """
     在识别区域内生成随机点击坐标
 
     Args:
-        box: 识别区域 Rect(x, y, w, h)
+        box: 识别区域 Rect(x, y, w, h) 或 [x, y, w, h]
 
     Returns:
         (x, y) 随机坐标元组
     """
-    rx = random.randint(box.x, box.x + box.w)
-    ry = random.randint(box.y, box.y + box.h)
-    # logger.debug(f"随机点击坐标: ({rx}, {ry}), 区域: ({box.x}, {box.y}, {box.w}, {box.h})")
+    rect = _to_rect(box)
+    rx = random.randint(rect.x, rect.x + rect.w)
+    ry = random.randint(rect.y, rect.y + rect.h)
+    # logger.debug(f"随机点击坐标: ({rx}, {ry}), 区域: ({rect.x}, {rect.y}, {rect.w}, {rect.h})")
     return rx, ry
 
 
-def click_rect(context: Context, box: Rect):
+def click_rect(context: Context, box: Rect | list):
     """
     在识别区域内随机点击
 
@@ -36,7 +52,7 @@ def click_rect(context: Context, box: Rect):
 
     Args:
         context: Maa context
-        box: 识别区域 Rect(x, y, w, h)
+        box: 识别区域 Rect(x, y, w, h) 或 [x, y, w, h]
     """
     rx, ry = random_click_point(box)
     context.tasker.controller.post_click(rx, ry).wait()
