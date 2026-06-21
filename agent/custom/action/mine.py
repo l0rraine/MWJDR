9ef377b042397a353c9c1727838c1d7e77c01030
l0rraine <idn.lee@gmail.com>
2026-06-21 21:50:45 +0800

    feat: 添加挖矿识别逻辑，更新矿种处理和训练流程

--- agent/custom/action/mine.py
+++ agent/custom/action/mine.py
@@ -22,6 +22,27 @@ recall_region = [
 LAST_MINES = []
 CURRENT_MINES = []
 NEXT_MINE = ""
+MINES = ["肉", "木", "煤", "铁"]
+
+
+def get_current_mines(context: Context, img):
+    global MINES
+    m = []
+    for mine in MINES:
+        d = context.run_recognition(
+            "挖矿_识别在挖矿",
+            img,
+            {
+                "挖矿_识别在挖矿": {
+                    "recognition": "TemplateMatch",
+                    "template": f"{mine}.png",
+                    "roi": [11, 241, 47, 226],
+                }
+            },
+        )
+        if d and d.hit:
+            m.append(mine)
+    return m
 
 
 @AgentServer.custom_recognition("挖矿_识别队伍")
@@ -31,11 +52,13 @@ class MineRecoTeam(CustomRecognition):
         context: Context,
         argv: CustomRecognition.AnalyzeArg,
     ) -> Union[CustomRecognition.AnalyzeResult, Optional[RectType]]:
+        global CURRENT_MINES, LAST_MINES, NEXT_MINE, MINES
+
         img = context.tasker.controller.post_screencap().wait().get()
 
         detail = context.run_recognition("挖矿_识别队伍数量", img)
         if not detail or not detail.hit:
-            logger.debug("没找到匹配的3/3")
+            logger.debug("无法识别队伍数量")
             return CustomRecognition.AnalyzeResult(box=None, detail={})
         pattern = r"(\d+)\D(\d+)"
         res = re.match(pattern, detail.best_result.text)
@@ -46,36 +69,32 @@ class MineRecoTeam(CustomRecognition):
 
         if num1 == num2:
             logger.debug("队列已满")
+            if not LAST_MINES:
+                LAST_MINES = get_current_mines(context, img)
             return CustomRecognition.AnalyzeResult(box=None, detail={})
 
-        global CURRENT_MINES, LAST_MINES, NEXT_MINE
         CURRENT_MINES.clear()
-        mines = ["肉", "木", "煤", "铁"]
-        for mine in mines:
-            detail = context.run_recognition(
-                "挖矿_识别在挖矿",
-                img,
-                {
-                    "挖矿_识别在挖矿": {
-                        "recognition": "TemplateMatch",
-                        "template": f"{mine}.png",
-                        "roi": [11, 241, 47, 226],
-                    }
-                },
-            )
-            if detail and detail.hit:
-                CURRENT_MINES.append(mine)
+        CURRENT_MINES = get_current_mines(context, img)
+
         NEXT_MINE = None
+        # 算出本轮消失的矿（已采集完成）
         finished_mines = [item for item in LAST_MINES if item not in CURRENT_MINES]
-        for mine in mines:
-            # 每次都在列表里遍历查找
+        logger.debug("已采完矿：", finished_mines)
+
+        # 遍历矿序，筛选：不在当前矿 且 不是刚采完的矿
+        for mine in MINES:
             if mine not in CURRENT_MINES and mine not in finished_mines:
                 NEXT_MINE = mine
                 break
-        LAST_MINES = CURRENT_MINES
-        logger.debug(f"下个矿为{NEXT_MINE}")
+
+        logger.debug(
+            f"LAST_MINES:{LAST_MINES},CURRENT_MINES:{CURRENT_MINES},NEXT_MINE:{NEXT_MINE}"
+        )
         if NEXT_MINE:
             logger.info(f"下一个要挖： {NEXT_MINE}")
+            CURRENT_MINES.append(NEXT_MINE)
+            LAST_MINES = CURRENT_MINES
+            logger.debug(f"LAST_MINES:{LAST_MINES}")
             return CustomRecognition.AnalyzeResult(box=detail.box, detail={})
         return CustomRecognition.AnalyzeResult(box=None, detail={})
 
